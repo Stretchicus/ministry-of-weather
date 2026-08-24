369a3cb6b28aa5f8634ce44a54ca5c14f2dca03f
Jon <stretch1790@yahoo.com>
2026-08-24 16:32:26 +0100

    changed the port to 3006

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const now = () => new Date();
 const weather = createWeather({ db, fetchFn: global.fetch, now });
 const app = createApp({ db, now, weather });
 
-app.listen(3000, () => {
-  console.log('The Ministry of Weather is open at http://localhost:3000');
+const port = Number(process.env.PORT) || 3006;
+
+app.listen(port, () => {
+  console.log(`The Ministry of Weather is open at http://localhost:${port}`);
 });
